fe7be1fa787971ed3ef89c4785adb5ab224b4712
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-05-04 16:46:47 +0300

    add variables

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+alert("Hello world");
+
+// let, const,
+// var - застарілий
+// debugger;
+let age = 18;
+alert(age);
+age = 19;
+alert(age);
+
+const count = 10;
+// count = 11;
+alert(count);
+
+const PI = 3.14;

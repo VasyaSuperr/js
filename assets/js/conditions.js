a6fa5e645414b4ce5b80d68968751f534faeea2d
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-05-11 19:38:01 +0300

    if, else last

--- assets/js/conditions.js
+++ assets/js/conditions.js
@@ -113,22 +113,108 @@
 //     гілка true
 // }
 
-const age = 16;
+// const age = 16;
 
-if (age <= 18) {
-  alert("У вас обмежений доступ для послуг порталу");
-}
+// if (age <= 18) {
+//   alert("У вас обмежений доступ для послуг порталу");
+// }
 
-document.write(`<p>Вітаємо на порталі</p>`);
+// document.write(`<p>Вітаємо на порталі</p>`);
 
-const age1 = +prompt("Input age", "0");
+// const age1 = +prompt("Input age", "0");
 
-if (Number.isInteger(age1)) {
+// if (Number.isInteger(age1)) {
+//   console.log("Correct");
+// } else {
+//   console.log("Incorect. Input again");
+// }
+
+// if (Number.isInteger(age1) === false) {
+//   console.log("Incorect. Input again");
+// }
+
+// Логічні (булеві) операції
+
+// console.group("Таблиця істинності &&:");
+
+// console.log("true && true :>> ", true && true); // => true, якщо всі true
+// console.log("false && false :>> ", false && false);
+// console.log("true && false :>> ", true && false);
+// console.log("false && true :>> ", false && true);
+
+// console.groupEnd();
+
+// console.group("Таблиця істинності :");
+
+// console.log("true  true :>> ", true || true);
+// console.log("false  false :>> ", false || false); // => false, якщо всі false
+// console.log("true  false :>> ", true || false);
+// console.log("false  true :>> ", false || true);
+
+// console.groupEnd();
+
+// const isMale = true;
+// const isMarried = true;
+
+// const name = "Test";
+
+// if (isMale) {
+//   document.write(`<p>Hello Mr. ${name}</p>`);
+// } else if (!isMale && isMarried) {
+//   document.write(`<p>Hello Mrs. ${name}</p>`);
+// } else {
+//   document.write(`<p>Hello Ms. ${name}</p>`);
+// }
+
+// let appeal = "";
+
+// if (isMale) {
+//   appeal = "Mr";
+// } else if (isMarried) {
+//   appeal = "Mrs";
+// } else {
+//   appeal = "Ms";
+// }
+
+// document.write(`<p>Hello ${appeal} ${name}</p>`);
+
+// const price = 800;
+// const age = 66;
+// let discount = null;
+
+// if (age < 18 || age >= 65) {
+//   discount = price * 0.25;
+// }
+
+// document.write(`<p>${price - discount}</p>`);
+
+// const day = "friday";
+// const date = 13;
+
+// if (day === "friday" || date === 13) {
+//   document.write("<p>Вітаємо, у Вас знижка 10%</p>");
+// }
+
+// NaN === NaN => false
+// Number.isNaN()
+
+// Перевірка
+
+let value = 0;
+
+// if (value !== undefined && value !== null) {
+//   console.log("Correct");
+// }
+
+// false, null, undefined, '', NaN, 0/-0 - falsy - значення
+if (value) {
   console.log("Correct");
-} else {
-  console.log("Incorect. Input again");
 }
 
-if (Number.isInteger(age1) === false) {
-  console.log("Incorect. Input again");
-}
+const port = 0;
+
+const PORT = port || 5500; // false, null, undefined, '', NaN, 0/-0
+const PORT1 = port ?? 5500; // null, undefined - (nullish)
+
+console.log("PORT :>> ", PORT);
+console.log("PORT1 :>> ", PORT1);
